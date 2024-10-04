7308ecdef08d3152183e75d75fe78d62cc8e72a9
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-10-04 16:49:54 -0400

    Project picture update and server.js update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import dotenv from 'dotenv';
+
 
 const express = require('express')
 const app = express()
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
         {PROJECTS.map((project, index) => (
             <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
                 <div className='w-full lg:w-1/4'>
-                <img src={project.image} width={150} height={150} alt={project.title} className='mb-6 rounded'/>
+                <img src={project.image} width={300} height={300} alt={project.title} className='mb-6 rounded'/>
             </div>
                 <div className='w-full max-w-xl lg:w-3/4'>
                     <h6 className='mb-2 font-semibold'>{project.title}</h6>
